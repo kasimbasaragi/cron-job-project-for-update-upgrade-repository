251f56d320d8ada08e6a0d1b5afd84325154c9cf
Kashim Basaragi <110844391+kasimbasaragi@users.noreply.github.com>
2022-12-22 13:01:55 +0530

    Update yum_repository_updater.sh

--- yum_repository_updater.sh
+++ yum_repository_updater.sh
@@ -1,6 +1,10 @@
 ## this is our script file ##
 --------------------------------------------------------------------------------
+#vim yum_repository_updater.sh [press enter]
+--------------------------------------------------------------------------------
 #!/bin/bash
 
 yum update
 yum upgrade
+
+[press ESC key then type :wq!]
